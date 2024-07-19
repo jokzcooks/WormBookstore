f8af172f99b431023de630fa7468535010a71357
jokzcooks <jokzcooks@gmail.com>
2024-07-19 13:45:29 -0400

    Implement confirm registration page

--- client/src/containers/ConfirmRegistration/index.jsx
+++ client/src/containers/ConfirmRegistration/index.jsx
@@ -1,27 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-const ConfirmRegistration = ({}) => {
+const ConfirmRegistration = ({tryConfirm}) => {
 
     const navigate = useNavigate()
     const location = useLocation()
 
-    // useEffect(() => {
-    //     const returnTo = new URLSearchParams(location.search).get("returnTo");
-    //     setTimeout(() => {
-    //         if (returnTo) {
-    //             navigate(returnTo)
-    //         } else {
-    //             navigate("/")
-    //         }         
-    //     }, 3000);
-    // }, [])
-    
+    const [code, setCode] = useState("")
+
+    const returnTo = new URLSearchParams(location.search).get("returnTo");
+
+    useEffect(() => {
+        if (code.length < 6) return
+        const tryCon = async () => {
+            const res = await tryConfirm(code)
+            if (!res || res == false) setCode("")
+            if (returnTo) {
+                navigate(returnTo)
+            } else {
+                navigate("/")
+            }         
+        }
+        tryCon()
+    }, [code])
 
     return (
 
-        <div style={{marginTop: "8%"}}>
-            Successfully registered account! Redirecting...
+        <div className="confirmReg">
+            <div>
+                <h2>Complete Your Registration</h2>
+                <p>Check your email for a confirmation code!</p>
+                <form>
+                    <input value={code} maxLength={6} onChange={e => setCode(e.target.value)} type="text" name="" id="" placeholder="123456" />
+                </form>
+            </div>
+
         </div>
 
     )
