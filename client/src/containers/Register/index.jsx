958e8321ad2da327edc469a2501563495cda42b3
jokzcooks <jokzcooks@gmail.com>
2024-07-18 22:53:08 -0400

    Refactor & further implement registration

--- client/src/containers/Register/index.jsx
+++ client/src/containers/Register/index.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-const RegisterPage = ({login}) => {
+const RegisterPage = ({register}) => {
 
-    const [fullName, setFullName] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
@@ -11,24 +12,34 @@ const RegisterPage = ({login}) => {
     const location = useLocation()
     const returnTo = new URLSearchParams(location.search).get("returnTo");
     console.log(returnTo)
-    const createAccount = () => {
-        login()
-        navigate(`/confirmReg${returnTo ? "?returnTo=" + returnTo : ""}`)
+
+    const createAccount = async () => {
+        console.log("Creating account!", firstName, lastName, email, password)
+        const res = await register(firstName, lastName, email, password)
+        if (res == true) navigate(`/confirmReg${returnTo ? "?returnTo=" + returnTo : ""}`)
     }
 
     return (
         <div className="loginPage">
-            <div className="chokeLogin">
+            <form className="chokeLogin" onSubmit={e => createAccount()}>
                 <p className="title">Register for a <span>Worm</span> Account</p>
                 <div className="hr"></div>
-                <p className="label">Full Name <span>*</span></p>
-                <input value={fullName} onChange={e => setFullName(e.target.value)} type="text" name="" id="" placeholder="bookworm@gmail.com" />
+                <div className="fullName">
+                    <div>
+                        <p className="label">First Name <span>*</span></p>
+                        <input value={firstName} onChange={e => setFirstName(e.target.value)} type="text" name="" id="" placeholder="John" />
+                    </div>
+                    <div>
+                        <p className="label">Full Name <span>*</span></p>
+                        <input value={lastName} onChange={e => setLastName(e.target.value)} type="text" name="" id="" placeholder="Doe" />
+                    </div>
+                </div>
                 <p className="label">Email <span>*</span></p>
                 <input value={email} onChange={e => setEmail(e.target.value)} type="text" name="" id="" placeholder="bookworm@gmail.com" />
                 <p className="label">Password <span>*</span></p>
                 <input value={password} onChange={e => setPassword(e.target.value)} type="password" name="" id="" placeholder="Password" />
-                <button onClick={e => createAccount()} className="login">Create Account</button>
-            </div>
+                <button type="submit" className="login">Create Account</button>
+            </form>
         </div>
     )
 }
