013491fb740c1ae860e51005bb99a80726f28e85
jamielockaby <93889284+jamielockaby@users.noreply.github.com>
2024-07-30 13:42:55 -0400

    Update auth.js - isAdmin

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models/User');
+const { User } = require('../db/models/User'); // Adjust the path according to your file structure
 
+// Middleware to check if the user is logged in
 const isLoggedIn = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) {
     return res.status(401).send({ error: 'Authentication token is missing' });
   }
-  
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-    
+
     if (!user) {
-      throw new Error("No user found");
+      throw new Error('No user found');
     }
-    
+
     req.token = token;
     req.user = user;
     next();
@@ -23,4 +24,12 @@ const isLoggedIn = async (req, res, next) => {
   }
 };
 
-module.exports = isLoggedIn;
+// Middleware to check if the user is an admin
+const isAdmin = async (req, res, next) => {
+  if (!req.user.admin) {
+    return res.status(403).json({ message: 'Access denied. Admins only.' });
+  }
+  next();
+};
+
+module.exports = { isLoggedIn, isAdmin };
